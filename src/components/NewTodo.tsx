2213d7d8c7a55478eed0ed1e2941cbc4d02ac31e
Judith van Leersum <judithvleersum@gmail.com>
2022-08-05 16:05:58 +0200

    Added logic to remove a prop

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import classes from './NewTodo.module.css';
 
 const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
@@ -16,7 +17,7 @@ const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
     todoTextInputRef.current!.value = "";
   }
 
-  return <form onSubmit={addTodoHandler}>
+  return <form onSubmit={addTodoHandler} className={classes.form}>
     <label htmlFor="text">Todo Text</label>
     <input type="string" is="text" ref={todoTextInputRef}/>
     <button>Create Todo</button>
